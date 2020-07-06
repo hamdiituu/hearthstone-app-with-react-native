73cde22799c6939d69b767872f892505fdd74302
Hamdi Tuğ <hamditug@apple-MacBook.local>
2020-07-06 20:21:55 +0300

    util setting added

---
+++ src/utils/Constants.js
@@ -0,0 +1,9 @@
+export const BASE_URL = 'https://omgvamp-hearthstone-v1.p.rapidapi.com'; //base api url
+
+export const API_KEY = 'c30c3be00fmsh9f48a31b6feb4d3p1675b7jsn4af2a7fae9b0'; // api key
+
+//common header
+export const API_HEADER = {
+  'x-rapidapi-host': 'omgvamp-hearthstone-v1.p.rapidapi.com',
+  'x-rapidapi-key': API_KEY,
+};
---
+++ src/utils/Networking.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import * as Constants from './Constants';
+
+//Common req-res model
+
+const axiosBase = axios.create({
+  baseURL: Constants.BASE_URL,
+  headers: Constants.API_HEADER,  //standart header
+});
+
+export const get = (url, params) => {
+  return axiosBase.get(url, {
+    params,
+  });
+};
+
+export const post = (url, params) => {
+  return axiosBase.post(url, qs.stringify(params)); //qs -> jsonConvert
+};
